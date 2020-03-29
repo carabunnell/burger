754a23e5389467e262956084f055f0b9347307ae
Cara Bunnell <carabunnell@gmail.com>
2020-03-28 22:45:15 -0700

    initial commit with almost complete code

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Cheesy');
+INSERT INTO burgers (name) VALUES ('Fresh');
+INSERT INTO burgers (name, devoured) VALUES ('in-n-out', true);
+INSERT INTO burgers (name) VALUES ('Greesy');
